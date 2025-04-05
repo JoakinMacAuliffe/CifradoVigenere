8eda4a523016ec1141366aff4a0513fd4415ede0
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-04-05 18:05:54 -0300

    Probar encrypt() y decrypt()

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 //MAIN CLASS JUST FOR TESTING PURPOSES
 
-package classes;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        String key = "874365"; //En el abedecedario, 1234 sería abcd
+        System.out.println("Key: " + key);
+
+        Scanner sc = new Scanner(System.in);
+        BigVigenere bigVigenere = new BigVigenere(key);
+
+        //Pruebas para metodo encrypt()
+
+        System.out.print("Inputted message: ");
+        String message = sc.nextLine();
+
+        String encryptedMessage = bigVigenere.encrypt(message);
+        System.out.println("Encrypted message: " + encryptedMessage);
+
+        //Pruebas para metodo decrypt()
+
+        message = bigVigenere.decrypt(encryptedMessage);
+        System.out.println("Encrypted and decrypted message: " + message);
 
     }
 
